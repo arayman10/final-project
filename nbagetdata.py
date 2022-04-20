d0f8beec74bb94c5223cbe78ed82b2eb5db3d7f3
arayman10 <arayman@umich.edu>
2022-04-20 19:49:30 -0400

    Update nbagetdata.py

--- nbagetdata.py
+++ nbagetdata.py
@@ -12,6 +12,7 @@ def readInData(url):
         data = resp.text
         lst = json.loads(data)['data']  
     return lst  
+
 def setUpDatabase(db_name):
     path = os.path.dirname(os.path.abspath(__file__))
     conn = sqlite3.connect(path + '/' + db_name)
@@ -19,25 +20,16 @@ def setUpDatabase(db_name):
     return cur, conn
 
 def createWeightTable(data, cur, conn):
-    cur.execute('CREATE TABLE IF NOT EXISTS Detriotteams (id INTEGER PRIMARY KEY, NBA_id INTEGER UNIQUE, weight INTEGER')
+    cur.execute('CREATE TABLE Detriotteams (id INTEGER PRIMARY KEY, NBA_id INTEGER UNIQUE, weight INTEGER)')
     #url = 'https://www.balldontlie.io/api/v1/players/'
 
-    #count = 0 
-    '''
-    for i in range(1, 38):
-        query = {'per_page': '100', 'page': str(i)}
-        resp = requests.get(url, params = query)
-        data = resp.text
-        lst = json.loads(data)['data']
-        new_lst = []
-        '''
-
+ 
     for player in data:
         player_weight = player['weight_pounds']
         #if player['id'] == 9:
             #new_lst.append(player_weight)
     
-    NBA_id = player['id']
+        NBA_id = player['id']
     #add_to_data = cur.execute('SELECT NBA_id FROM IDs WHERE NBA_id = ?', (NBA_id,)).fetchone()
     #if add_to_data is None:
     cur.execute('INSERT OR IGNORE INTO Detriotteams (NBA_id, weight) VALUES (?, ?)', (NBA_id, player_weight))
