2f3c9fe1e4d2481b5e51ec7f21a29dd9ef7bea07
arayman10 <arayman@umich.edu>
2022-04-20 21:04:23 -0400

    Update nba2.py

--- nba2.py
+++ nba2.py
@@ -14,14 +14,17 @@ def setUpDatabase(db_name):
 def createNbaTable(cur, conn):
     cur.execute('CREATE TABLE IF NOT EXISTS Detroit_NBA (id INTEGER PRIMARY KEY, player_id INTEGER, weight INTEGER)')
     conn.commit()
-
+lst = ['Bagley', 'Saddiq', 'Cunningham', 'Hamidou', 'Carsen', 'Garza', 'Jerami', 'Killian', 'Frank', 'Joseph', 'Braxton', 'Saben', 'Livers', 'McGruder', 'Olynyk', 'Jamorko', 'Stewart']
 def getPlayerData():
     resp = requests.get('https://www.balldontlie.io/api/v1/players/')
     data = json.loads(resp.text)
     print(data)
     dict = {}
-    
-    
+    #for i in data['data']:
+        #for x in i:
+            
+
+
 
 def main():
     cur, conn = setUpDatabase('nba.db')
