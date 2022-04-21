15fea5eb78b3bd07edc8f7a4ca235561f1b7dceb
arayman10 <arayman@umich.edu>
2022-04-20 21:25:57 -0400

    Update nba2.py

--- nba2.py
+++ nba2.py
@@ -3,6 +3,8 @@ import sqlite3
 import os
 import json
 import time
+
+from sklearn.preprocessing import PolynomialFeatures
  
 
 def setUpDatabase(db_name):
@@ -15,13 +17,22 @@ def createNbaTable(cur, conn):
     cur.execute('CREATE TABLE IF NOT EXISTS Detroit_NBA (id INTEGER PRIMARY KEY, player_id INTEGER, weight INTEGER)')
     conn.commit()
 lst = ['Bagley', 'Saddiq', 'Cunningham', 'Hamidou', 'Carsen', 'Garza', 'Jerami', 'Killian', 'Frank', 'Joseph', 'Braxton', 'Saben', 'Livers', 'McGruder', 'Olynyk', 'Jamorko', 'Stewart']
-def getPlayerData():
-    resp = requests.get('https://www.balldontlie.io/api/v1/players/')
+full_names = ['Marvin Bagley III', 'Saddiq Bey', 'Cade Cunningham', 'Hamidou Diallo', 'Carsen Edwards', ' Luka Garza', 'Jerami Grant', 'Killian Hayes', 'Frank Jackson', 'Cory Joseph', 'Braxton Key', 'Saben Lee', 'Isaiah Livers', 'Rodney McGruder', 'Kelly Olynyk', 'Jamorko Pickett', 'Isaiah Stewart']
+players = {}
+def getPlayerData(full_name, single):
+    resp = requests.get(f'https://www.balldontlie.io/api/v1/players/?search={single}')
     data = json.loads(resp.text)
-    print(data)
-    dict = {}
-    #for i in data['data']:
-        #for x in i:
+    for i in data['data']:
+        first = i["first_name"]
+        last = i["last_name"]
+        total = first + last
+        if full_name == total:
+            weight = i["weight_pounds"]
+            players[full_name] = weight
+    print(players)
+
+      
+    
             
 
 
@@ -29,5 +40,6 @@ def getPlayerData():
 def main():
     cur, conn = setUpDatabase('nba.db')
     createNbaTable(cur, conn)
-    getPlayerData()
+    for i in range(len(full_names)):
+        getPlayerData(full_names[i], lst[i])
 main()
