d75fdb00371738b9093fcc14391cdc7fd3295a2b
arayman10 <arayman@umich.edu>
2022-04-20 21:31:39 -0400

    Update nba2.py

--- nba2.py
+++ nba2.py
@@ -18,28 +18,27 @@ def createNbaTable(cur, conn):
     conn.commit()
 lst = ['Bagley', 'Saddiq', 'Cunningham', 'Hamidou', 'Carsen', 'Garza', 'Jerami', 'Killian', 'Frank', 'Joseph', 'Braxton', 'Saben', 'Livers', 'McGruder', 'Olynyk', 'Jamorko', 'Stewart']
 full_names = ['Marvin Bagley III', 'Saddiq Bey', 'Cade Cunningham', 'Hamidou Diallo', 'Carsen Edwards', ' Luka Garza', 'Jerami Grant', 'Killian Hayes', 'Frank Jackson', 'Cory Joseph', 'Braxton Key', 'Saben Lee', 'Isaiah Livers', 'Rodney McGruder', 'Kelly Olynyk', 'Jamorko Pickett', 'Isaiah Stewart']
+
 players = {}
-def getPlayerData(full_name, single):
+
+def getPlayerData(full_name, single, dict):
+    
     resp = requests.get(f'https://www.balldontlie.io/api/v1/players/?search={single}')
     data = json.loads(resp.text)
     for i in data['data']:
         first = i["first_name"]
         last = i["last_name"]
-        total = first + last
+        total = first + " " + last
         if full_name == total:
             weight = i["weight_pounds"]
-            players[full_name] = weight
-    print(players)
-
-      
-    
-            
-
-
+            dict[full_name] = weight
+    return None
 
 def main():
     cur, conn = setUpDatabase('nba.db')
     createNbaTable(cur, conn)
     for i in range(len(full_names)):
-        getPlayerData(full_names[i], lst[i])
+        getPlayerData(full_names[i], lst[i], players)
 main()
+
+print(players)
