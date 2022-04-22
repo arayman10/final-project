181adf00d89ee738dcc45f07baa00cca49661ac3
arayman10 <arayman@umich.edu>
2022-04-22 19:59:23 -0400

    Update nba2.py

--- nba2.py
+++ nba2.py
@@ -4,7 +4,6 @@ import os
 import json
 import time
 
-from sklearn.preprocessing import PolynomialFeatures
  
 
 def setUpDatabase(db_name):
@@ -16,22 +15,24 @@ def setUpDatabase(db_name):
 def createNbaTable(cur, conn):
     cur.execute('CREATE TABLE IF NOT EXISTS Detroit_NBA (id INTEGER PRIMARY KEY, player_name TEXT, weight INTEGER)')
     conn.commit()
-lst = ['Bagley', 'Saddiq', 'Cunningham', 'Hamidou', 'Carsen', 'Garza', 'Jerami', 'Killian', 'Frank', 'Joseph', 'Braxton', 'Saben', 'Livers', 'McGruder', 'Olynyk', 'Jamorko', 'Stewart']
-full_names = ['Marvin Bagley III', 'Saddiq Bey', 'Cade Cunningham', 'Hamidou Diallo', 'Carsen Edwards', ' Luka Garza', 'Jerami Grant', 'Killian Hayes', 'Frank Jackson', 'Cory Joseph', 'Braxton Key', 'Saben Lee', 'Isaiah Livers', 'Rodney McGruder', 'Kelly Olynyk', 'Jamorko Pickett', 'Isaiah Stewart']
 
-players = {}
 
-def getPlayerData(full_name, single, dict):
-    
-    resp = requests.get(f'https://www.balldontlie.io/api/v1/players/?search={single}')
-    data = json.loads(resp.text)
-    for i in data['data']:
-        first = i["first_name"]
-        last = i["last_name"]
-        total = first + " " + last
-        if full_name == total:
+player_weight = {}
+
+def getPlayerData(dict):
+    for i in range(1,38):
+        querystring = {'per_page': '100', 'page': str(i)}
+        resp = requests.get('https://www.balldontlie.io/api/v1/players/', params=querystring)
+        data = json.loads(resp.text)
+        for i in data['data']:
+            first = i["first_name"]
+            last = i["last_name"]
+            full_name = first + " " + last
             weight = i["weight_pounds"]
-            dict[full_name] = weight
+            try:
+                dict[full_name] = int(weight)
+            except:
+                print('Not a number')
     return None
 
 def addPlayerWeightsToTable(cur, conn, dict):
@@ -39,11 +40,10 @@ def addPlayerWeightsToTable(cur, conn, dict):
         cur.execute('INSERT INTO Detroit_NBA (player_name, weight) VALUES (?, ?)', (i[0], i[1]))
     conn.commit()
 def main():
-    cur, conn = setUpDatabase('nba.db')
+    cur, conn = setUpDatabase('nba2.db')
     createNbaTable(cur, conn)
-    for i in range(len(full_names)):
-        getPlayerData(full_names[i], lst[i], players)
-    addPlayerWeightsToTable(cur, conn, players)
+    getPlayerData(player_weight)
+    addPlayerWeightsToTable(cur, conn, player_weight)
 main()
 
-print(players)
+
