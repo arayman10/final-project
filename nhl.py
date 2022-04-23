394720e6d61be65db8723d5943cf5a8d0926ea18
Your Name Heresasha <sashasto@umich.edu>
2022-04-22 22:30:13 -0400

    Update nhl.py

--- nhl.py
+++ nhl.py
@@ -16,6 +16,28 @@ def create_nhl_table(cur, conn):
     cur.execute('CREATE TABLE IF NOT EXISTS Detroit_NHL (id INTEGER PRIMARY KEY, player_name TEXT, weight INTEGER)')
     conn.commit()
 
+def create_team_table(cur, conn):
+    cur.execute('CREATE TABLE IF NOT EXISTS TeamNames (id INTEGER, team TEXT)')
+    conn.commit()
+
+def team_data(cur, conn):
+    data = requests.get('https://statsapi.web.nhl.com/api/v1/teams')
+    load_data = json.loads(data.text)   
+    teams = []
+    for x in load_data["teams"]:
+        id = x['id']
+        name = x['name']
+        teams.append((id,name))
+    count = 0
+    for i in range(len(teams)):
+        if count > 24:
+            break
+        if cur.execute('SELECT team FROM TeamNames WHERE id = ? AND team = ?', (teams[i][0], teams[i][1])).fetchone() == None:
+            cur.execute('INSERT OR IGNORE INTO TeamNames (id, team) VALUES (?, ?)', (teams[i][0], teams[i][1]))
+            count += 1
+    conn.commit()
+    return None
+
 def get_player_data():
     data = requests.get('https://records.nhl.com/site/api/player')
     load_data = json.loads(data.text)
@@ -41,6 +63,8 @@ def addPlayerWeightsToTable(cur, conn, tup_lst):
 def main():
     cur,conn = setUpDatabase('nhl.db')
     create_nhl_table(cur, conn)
+    create_team_table(cur,conn)
+    team_data(cur,conn)
     players_data = get_player_data()
     addPlayerWeightsToTable(cur, conn, players_data)
     
