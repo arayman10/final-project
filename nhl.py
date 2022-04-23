75ee8850318fcdba5a7f38b312ca4678e35dd88d
Your Name Heresasha <sashasto@umich.edu>
2022-04-22 21:00:38 -0400

    Update nhl.py

--- nhl.py
+++ nhl.py
@@ -17,27 +17,29 @@ def create_nhl_table(cur, conn):
     conn.commit()
 
 def get_player_data():
-    data = requests.get('https://records.nhl.com/site/api/player/byTeam/17')
+    data = requests.get('https://records.nhl.com/site/api/player')
     load_data = json.loads(data.text)
-    #print(load_data)
-    player_weight = {}
-    for i in load_data["data"]:
-        name = i['fullName']
-        weight = i['weight']
-        player_weight[name] = int(weight)
+    player_weight = []
+    for i in range(25):
+        dict = load_data["data"][i]
+        name = dict['fullName']
+        weight = dict['weight']
+        tup = (name, weight)
+        player_weight.append(tup)
+    print(player_weight)
     return player_weight
 
-def addPlayerWeightsToTable(cur, conn, dict):
-    for i in dict.items():
-        cur.execute('INSERT INTO Detroit_NHL (player_name, weight) VALUES (?, ?)', (i[0], i[1]))
-    conn.commit()
+#def addPlayerWeightsToTable(cur, conn, tup_lst):
+    #for i in tup_lst:
+        #cur.execute('INSERT INTO Detroit_NHL (player_name, weight) VALUES (?, ?)', (i[0], i[1]))
+    #conn.commit()
 
 
 def main():
     cur,conn = setUpDatabase('nhl.db')
     create_nhl_table(cur, conn)
-    players_data = get_player_data()
-    addPlayerWeightsToTable(cur, conn, players_data)
+    get_player_data()
+    #addPlayerWeightsToTable(cur, conn, players_data)
     
 main()
 
