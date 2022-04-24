a46236b9eb2b8ab01534ef313e61e4fab36378a6
Your Name Heresasha <sashasto@umich.edu>
2022-04-24 16:39:00 -0400

    Update nhl.py

--- nhl.py
+++ nhl.py
@@ -17,7 +17,7 @@ def create_nhl_table(cur, conn):
     conn.commit()
 
 def create_team_table(cur, conn):
-    cur.execute('CREATE TABLE IF NOT EXISTS TeamNames (id INTEGER, team TEXT)')
+    cur.execute('CREATE TABLE IF NOT EXISTS TeamNames (id INTEGER PRIMARY KEY, team TEXT, data_id INTEGER)')
     conn.commit()
 
 def team_data(cur, conn):
@@ -28,37 +28,46 @@ def team_data(cur, conn):
         id = x['id']
         name = x['name']
         teams.append((id,name))
-    print(teams)
+    
     count = 0
     for i in range(len(teams)):
         if count > 24:
             break
-        if cur.execute('SELECT team FROM TeamNames WHERE id = ? AND team = ?', (teams[i][0], teams[i][1])).fetchone() == None:
-            cur.execute('INSERT OR IGNORE INTO TeamNames (id, team) VALUES (?, ?)', (teams[i][0], teams[i][1]))
+        if cur.execute('SELECT team FROM TeamNames WHERE team = ? AND data_id = ?', (teams[i][1], teams[i][0])).fetchone() == None:
+            cur.execute('INSERT OR IGNORE INTO TeamNames (team, data_id) VALUES (?, ?)', (teams[i][1], teams[i][0]))
             count += 1
     conn.commit()
     return None
 
-def get_player_data():
+def get_player_data(cur, conn):
     data = requests.get('https://records.nhl.com/site/api/player')
     load_data = json.loads(data.text)
     player_weight = []
-    for i in range(150):
+    for i in range(275):
         dict = load_data["data"][i]
         name = dict['fullName']
         weight = dict['weight']
-        team_id = dict['careerTeamId']
-        tup = (name, weight, team_id)
+        team_num = dict['careerTeamId']
+        tup = (name, weight, team_num)
         player_weight.append(tup)
-    return player_weight
+    new_player_weight = []
+    for item in player_weight:
+        cur.execute('SELECT id, data_id FROM TeamNames')
+        for row in cur:
+            if item[2] == row[1]:
+                team_id = row[0]
+                new_player_weight.append((item[0], item[1], item[2], team_id))
+    conn.commit()
+    print(len(new_player_weight))    
+    return new_player_weight
 
 def addPlayerWeightsToTable(cur, conn, tup_lst):
     count = 0
     for i in range(len(tup_lst)):
         if count > 24:
             break
-        if cur.execute('SELECT player_name FROM Detroit_NHL WHERE player_name = ? AND weight = ? AND team_id = ?', (tup_lst[i][0], tup_lst[i][1], tup_lst[i][2])).fetchone() == None:
-            cur.execute('INSERT OR IGNORE INTO Detroit_NHL (player_name, weight, team_id) VALUES (?, ?, ?)', (tup_lst[i][0], tup_lst[i][1], tup_lst[i][2]))
+        if cur.execute('SELECT player_name FROM Detroit_NHL WHERE player_name = ? AND weight = ? AND team_id = ?', (tup_lst[i][0], tup_lst[i][1], tup_lst[i][3])).fetchone() == None:
+            cur.execute('INSERT OR IGNORE INTO Detroit_NHL (player_name, weight, team_id) VALUES (?, ?, ?)', (tup_lst[i][0], tup_lst[i][1], tup_lst[i][3]))
             count += 1
     conn.commit()
 
@@ -67,7 +76,7 @@ def main():
     create_nhl_table(cur, conn)
     create_team_table(cur,conn)
     team_data(cur,conn)
-    players_data = get_player_data()
+    players_data = get_player_data(cur, conn)
     addPlayerWeightsToTable(cur, conn, players_data)
     
 main()
