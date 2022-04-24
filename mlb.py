ae6270b2ca7094f0172e759a5b23792d3fad1a80
arayman10 <arayman@umich.edu>
2022-04-24 16:02:11 -0400

    Update mlb.py

--- mlb.py
+++ mlb.py
@@ -1,5 +1,7 @@
 import requests
 import json
+import os
+import sqlite3
 '''
 def get_tigers_weight_data(player):
     base_url = "http://lookup-service-prod.mlb.com/json/named.search_player_all.bam?sport_code='mlb'&active_sw='Y'&name_part=" + "'" + player + "'"
@@ -9,15 +11,40 @@ def get_tigers_weight_data(player):
     
 data = (get_tigers_weight_data('jason foley'))
 '''
-def get_tigers_players():
+def setUpDatabase(db_name):
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path + '/' + db_name)
+    cur = conn.cursor()
+    return cur, conn
+
+def createMlbTable(cur, conn):
+    cur.execute('CREATE TABLE IF NOT EXISTS MLB_Players (id INTEGER PRIMARY KEY, player_name TEXT, weight INTEGER)')
+    conn.commit()
+
+player_weight = []
+
+def get_tigers_players(lst):
     base_url = "http://lookup-service-prod.mlb.com/json/named.roster_40.bam?team_id='116'"
     res = requests.get(base_url)
     data = json.loads(res.text)
+    for i in data['roster_40']['queryResults']['row']:
+        first = i['name_first']
+        last = i['name_last']
+        total = first + ' ' + last
+        weight = i['weight']
+        try:
+            lst.append((total, int(weight)))
+        except:
+            None
+    return None
+
+'''
     new_lst = data['roster_40']['queryResults']['row']
     weight_list = []
     for item in new_lst:
        weight_list.append(item['weight'])
-    return weight_list
+    '''
+'''    
 tigers_rosters = get_tigers_players()
 #print(tigers_rosters)
 count = len(tigers_rosters)
@@ -28,8 +55,18 @@ for number in tigers_rosters:
     tigers_rosters_int.append(int(number))
 #print(total_weight)
 tigers_avg_weight = total_weight/count
+'''
+def add_to_table(cur, conn, lst):
+    count = 0
+    for i in range(len(lst)):
+        if count > 24:
+            break
+        if cur.execute('SELECT player_name FROM MLB_Players WHERE player_name = ? AND weight = ?', (lst[i][0], lst[i][1])).fetchone() == None:
+            cur.execute('INSERT OR IGNORE INTO MLB_Players (player_name, weight) VALUES (?, ?)', (lst[i][0], lst[i][1]))
+            count += 1
+    conn.commit()
 
-
+'''
 #GRAPHING
 import matplotlib.pyplot as plt
 plt.hist(tigers_rosters_int, bins= 12)
@@ -76,3 +113,11 @@ plt.xlabel("Team", fontsize = 12 )
 plt.ylabel("Average Player Weight", fontsize= 12)
 plt.bar(x_title, y_title, color = color_lst)
 plt.show()
+'''
+
+def main():
+    cur, conn = setUpDatabase('mlb.db')
+    createMlbTable(cur, conn)
+    get_tigers_players(player_weight)
+    add_to_table(cur,conn,player_weight)
+main()
