c2455cbb85f58590173babb20226357b8e36270a
Your Name Heresasha <sashasto@umich.edu>
2022-04-25 17:38:17 -0400

    Update nhl.py

--- nhl.py
+++ nhl.py
@@ -68,19 +68,15 @@ def addPlayerWeightsToTable(cur, conn, tup_lst):
             count += 1
     conn.commit()
 
-def get_avg_weight(players_data):   
-    weight_lst = []
+weight_lst = []
+
+def get_avg_weight(players_data, weight_lst):   
     for i in players_data:
         weight = i[1]
         weight_lst.append(weight)
-    print(sorted(weight_lst))
-    count = len(weight_lst)
-    total_weight = 0
-    for number in weight_lst:
-        total_weight += int(number)
-    avg_weight = total_weight/count
-    print(avg_weight)
-    return avg_weight
+    weight_lst = sorted(weight_lst)
+    min_max = (weight_lst[0], weight_lst[-1])
+    return min_max
 
 def avg_weight(cur, conn):
     cur.execute('SELECT AVG(weight) FROM Detroit_NHL')
@@ -116,7 +112,7 @@ def main():
     #teams = team_data(cur,conn)
     players_data = get_player_data(cur, conn)
    #addPlayerWeightsToTable(cur, conn, players_data)
-    get_avg_weight(players_data)
+    get_avg_weight(players_data, weight_lst)
     print(avg_weight(cur, conn))
     #for num in range(len(teams)):
         #players_per_team(team_count, num+1, cur, conn)
