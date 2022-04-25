6b295a8369b748e47a9f88893760999492838e59
Jbrookstone <jbrookst@umich.edu>
2022-04-25 17:50:31 -0400

    Update nhl.py

--- nhl.py
+++ nhl.py
@@ -95,7 +95,7 @@ def players_per_team(dict, num, cur, conn):
             dict[team[0][0]] += 1
     conn.commit()
     return None
-'''
+
 #create pie chart
 labels = 'New York Rangers', 'Philadelphia Flyers', 'Pittsburgh Penguins', 'Boston Bruins', 'Montréal Canadiens', 'Toronto Maple Leafs', 'Washington Capitals', 'Chicago Blackhawks', 'Detroit Red Wings', 'St. Louis Blues', 'Edmonton Oilers', 'Vancouver Canucks', 'Los Angeles Kings'
 sizes = [16, 2, 3, 17, 15, 20, 3, 13, 15, 1, 1, 2, 2]
@@ -104,7 +104,7 @@ plt.pie(sizes, labels=labels, autopct='%1.1f%%', labeldistance=0.85, radius=5)
 plt.title("Players Per NHL Team")
 plt.axis('equal')
 plt.show()
-'''
+
 def main():
     cur,conn = setUpDatabase('final.db')
     create_nhl_table(cur, conn)
